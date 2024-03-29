40a7afd7535522964b8d46bb77e6f21a6ed4481e
ofagbuji <jidexl21@gmail.com>
2024-03-29 11:41:20 +0100

    activate timeline section

--- template-parts/body-timeline.php
+++ template-parts/body-timeline.php
@@ -1,17 +1,19 @@
-<?php $section_name = end(explode('-',basename(__FILE__, '.php'))); ?>
+<?php $section_name = end(explode('-',basename(__FILE__, '.php')));
+
+    $cat = get_category_by_slug($section_name);
+    $title = ($cat) ? $cat->cat_name: 'Timeline';
+    $cat_id = ($cat) ? $cat->cat_ID : 9999;
+    $args = array(
+        'posts_per_page'   => -1,
+        'cat'         =>  $cat_id,
+        'order'            => 'DESC',
+    );
+  ?>
 <!-- timeline -->
 
 <div class="bg-peach py-5">
 <div class="container">
-<?php 
-    $cat = get_category_by_slug($section_name);
-    $title = ($cat) ? $cat->cat_name: 'Timeline';
-    $args = array(
-        'posts_per_page'   => -1,
-        'cat'         => $cat->cat_ID,
-        'order'            => 'DESC',
-    );
-  ?>
+
   <div class="page-header">
       <h1 id="timeline"><?php print $title; ?></h1>
   </div>
@@ -39,7 +41,24 @@
           </div>
         </div>
       </li>
-    <?php $cnt++; endwhile; endif;?>
+    <?php $cnt++; endwhile; else:?>
+      <li>
+        <div class="timeline-badge danger">
+         <i class="glyphicon glyphicon-time"></i>
+        </div>
+        <div class="timeline-panel">
+          <div class="timeline-heading">
+            <h4 class="timeline-title">Add a timeline item</h4>
+            <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> <?php echo date(get_option('date_format')); ?></small></p>
+          </div>
+          <div class="timeline-body">
+            <p>
+              Timeline content comes here. to use this section place items in a category called timeline, or a category that as a slug named timeline <br/>
+            </p>
+          </div>
+        </div>
+      </li>
+    <?php endif;?>
   </ul>
 </div>
 </div>
